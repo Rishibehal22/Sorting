eb1dd4dbdc50390fa430730641cb2e81b0cb9799
Rishibehal22 <113096572+Rishibehal22@users.noreply.github.com>
2022-09-22 14:19:50 +0530

    Update BubbleSort.java

--- src/firstcode/BubbleSort.java
+++ src/firstcode/BubbleSort.java
@@ -1,10 +1,27 @@
 package firstcode;
 
+
 public class BubbleSort {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n = 6;
+		int[] arr = {45, 65, 44, 32, 25, 15};
+		
+		for(int i = 0; i<n-1; i++) {
+			for(int j = 0; j<n-i-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp;
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		System.out.println("Sorted array is: ");
+		for(int k = 0; k< 6; k++) {
+			System.out.print(arr[k] + " ");
+		}
+		System.out.println();
 	}
 
 }
